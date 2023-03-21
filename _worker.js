ad859ef3e3b31a35701124c8565c3452dab2c90c
sun-lemon <64244854+sun-lemon@users.noreply.github.com>
2023-03-21 20:22:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,37 +1,3 @@
-const openAuth = true
-const username = 'admin'
-const password = 'MYproxy520'
-
-function unauthorized() {
-  return new Response('Unauthorized', {
-    headers: {
-      'WWW-Authenticate': 'Basic realm="goindex"',
-      'Access-Control-Allow-Origin': '*'
-    },
-    status: 401
-  });
-}
- 
-function parseBasicAuth(auth) {
-    try {
-      return atob(auth.split(' ').pop()).split(':');
-    } catch (e) {
-      return [];
-    }
-}
- 
-function doBasicAuth(request) {
-  const auth = request.headers.get('Authorization');
- 
-  if (!auth || !/^Basic [A-Za-z0-9._~+/-]+=*$/i.test(auth)) {
-    return false;
-  }
- 
-  const [user, pass] = parseBasicAuth(auth);
-  return user === username && pass === password;
-}
-
-
 export default {
     async fetch(request, env) {
       if (!doBasicAuth(request)) {
